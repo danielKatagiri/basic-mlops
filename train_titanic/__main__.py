d79f1dc24502e6560838ddf949e34e0a94c113aa
Daniel Izumi Katagiri <izumi.katagiri@gmail.com>
2022-12-02 19:56:50 -0300

    Finished training routine

--- train_titanic/__main__.py
+++ train_titanic/__main__.py
@@ -1,4 +1,8 @@
+from typing import List
+
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import StandardScaler
 
 from .logging import LOGGER
 
@@ -16,23 +20,64 @@ def download_data() -> pd.DataFrame:
     return df
 
 
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_data(df: pd.DataFrame, features: List[str], target: str) -> pd.DataFrame:
     """
     Preprocess the training data by selecting columns and normalizing values
     Parameters:
         df (pd.DataFrame): Raw dataset
+        features (List[str]): Columns used as features
+        target (str): Target column
     Returns:
         (pd.DataFrame): Preprocessed dataset
     """
 
     LOGGER.info("Starting dataset preprocessing.")
 
-    selected_columns = ["Pclass", "Sex", "Age", "Fare", "Survived"]
+    scaler = StandardScaler()
 
-    preprocessed_df = df.loc[:, selected_columns].assign(
-        Sex=lambda df_: df_["Sex"].map({"male": 0, "female": 1})
+    preprocessed_df = df.loc[:, features + [target]].assign(
+        Sex=lambda df_: df_["Sex"].map({"male": 0, "female": 1}),
+        Age=lambda df_: scaler.fit_transform(df_[["Age"]]),
+        Fare=lambda df_: scaler.fit_transform(df_[["Fare"]]),
     )
 
     LOGGER.info("Dataset preprocessing finished.")
 
     return preprocessed_df
+
+
+def train_model(
+    df: pd.DataFrame, features: List[str], target: str
+) -> GradientBoostingClassifier:
+    """
+    Train a gradient boosting classifier
+    Parameters:
+        df (pd.Dataframe): Training dataframe
+        features (List[str]): Columns used as features
+        target (str): Target column
+    Returns:
+        (GradientBoostingClassifier): Trained classifier model
+    """
+    LOGGER.info("Training classifier model.")
+
+    model = GradientBoostingClassifier()
+    model.fit(df[features], df[target])
+
+    LOGGER.info("Finished model training.")
+
+    return model
+
+
+def main():
+    """Main function to train titanic model"""
+    features = ["Pclass", "Sex", "Age", "Fare"]
+    target = "Survived"
+
+    df = download_data()
+    preprocessed_df = preprocess_data(df=df, features=features, target=target)
+    model = train_model(df=preprocessed_df, features=features, target=target)
+    print(model)
+
+
+if __name__ == "__main__":
+    main()
